f90e6acb86e4323b7668ec5e2998ab8d0c908853
djzhang <trujunzhang@gmail.com>
2020-05-13 07:57:39 +0800

    search_bar_ui

--- lib/src/ui/homepage.dart
+++ lib/src/ui/homepage.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:ieatta/src/bloc/bloc.dart';
 
+import 'search_bar_ui.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -9,7 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   MovieListBloc movieListBloc;
- 
+
   ScrollController controller = ScrollController();
 
   @override
@@ -23,35 +25,46 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Firebase firestore pagination"),),
-      body: StreamBuilder<List<DocumentSnapshot>>(
-        stream: movieListBloc.movieStream,
-        builder: (context, snapshot) {
-          if (snapshot.data != null) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              shrinkWrap: true,
-              controller: controller,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                          child: Text(snapshot.data[index]["displayName"].toString())),
-                      title: Text(snapshot.data[index]["address"]),
-                    ),
+    var streamBuilder = StreamBuilder<List<DocumentSnapshot>>(
+      stream: movieListBloc.movieStream,
+      builder: (context, snapshot) {
+        if (snapshot.data != null) {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            shrinkWrap: true,
+            controller: controller,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        child: Text(
+                            snapshot.data[index]["displayName"].toString())),
+                    title: Text(snapshot.data[index]["address"]),
                   ),
-                );
-              },
-            );
-          } else {
-            return CircularProgressIndicator();
-          }
-        },
-      ),
+                ),
+              );
+            },
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Firebase firestore pagination"),
+        ),
+        body: Column(
+          children: <Widget>[
+            SearchBarUI(
+                nextPageClick: nextPageClick,
+                refreshPageClick: refreshPageClick,
+                searchChangedText: searchChangedText),
+            streamBuilder,
+          ],
+        ));
   }
 
   void _scrollListener() {
@@ -61,4 +74,12 @@ class _HomePageState extends State<HomePage> {
       movieListBloc.fetchNextMovies();
     }
   }
+
+  void nextPageClick() {
+    movieListBloc.fetchNextMovies();
+  }
+
+  void refreshPageClick() {}
+
+  void searchChangedText() {}
 }
---
+++ lib/src/ui/search_bar_ui.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class SearchBarUI extends StatefulWidget {
+  SearchBarUI(
+      {Key key,
+      this.nextPageClick,
+      this.refreshPageClick,
+      this.searchChangedText
+      })
+      : super(key: key);
+
+  final Function nextPageClick;
+  final Function refreshPageClick;
+  final Function searchChangedText;
+
+  @override
+  _SearchBarUIState createState() => _SearchBarUIState();
+}
+
+class _SearchBarUIState extends State<SearchBarUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      child: Row(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(38.0),
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    offset: const Offset(0, 2),
+                    blurRadius: 8.0),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(32.0),
+                ),
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Icon(
+//                      Icons.gps_fixed,
+                      Icons.gps_not_fixed,
+                      size: 20,
+//                      color: Colors.blue
+                      color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16, top: 8, bottom: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(38.0),
+                  ),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        offset: const Offset(0, 2),
+                        blurRadius: 8.0),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 16, right: 16, top: 4, bottom: 4),
+                  child: TextField(
+                    onChanged: (String txt) {},
+                    style: const TextStyle(
+                      fontSize: 18,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'London...',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(38.0),
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    offset: const Offset(0, 2),
+                    blurRadius: 8.0),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(32.0),
+                ),
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  widget.nextPageClick();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Icon(
+//                      Icons.gps_fixed,
+                      Icons.next_week,
+                      size: 20,
+//                      color: Colors.blue
+                      color: Colors.blue),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
