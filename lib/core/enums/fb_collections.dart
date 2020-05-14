995c9818b95664d9fce850b628ad6ca87232d15a
djzhang <trujunzhang@gmail.com>
2020-05-14 18:20:18 +0800

    fbCollectionToString

---
+++ lib/core/enums/fb_collections.dart
@@ -0,0 +1,14 @@
+enum FBCollections{
+  Restaurants,
+  Cellular,
+  Offline,
+}
+
+fbCollectionToString(FBCollections path){
+  switch (path) {
+    case FBCollections.Restaurants:
+      return "restaurants";
+    default:
+      return "none";
+  }
+}
